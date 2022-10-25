e3985d11b0a11ac1d90572ef7e37269e82f09c72
ReynahD <wl2357@nyu.edu>
2022-10-25 17:04:33 -0400

    update

---
+++ My project/Assets/Chip.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chip : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public float timer;
+    public GameObject chipPrefab;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            var x = Random.Range(-2, 2);
+            var y = Random.Range(-2, 2);
+            offset = new Vector3(x, y, 0);
+            Instantiate(chipPrefab, spawnPoint.position + offset, chipPrefab.transform.rotation);
+        }
+    }
+}
